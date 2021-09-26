582dd3035e4800f714e38b08a9e49223a16f863b
Mohammad3125 <farsroid312@gmail.com>
2021-09-26 20:58:10 +0330

    perf: make 'invalidateAfter' an inline function

--- mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/utils/CustomViewHelperFunctions.kt
+++ mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/utils/CustomViewHelperFunctions.kt
@@ -59,7 +59,7 @@ val View.centerY get() = (this.measuredHeight * 0.5f)
 /**
  * This extension function invokes a block and invalidate the view afterwards.
  */
-fun View.invalidateAfter(block: (Unit) -> Unit) {
+inline fun View.invalidateAfter(block: (Unit) -> Unit) {
     block(Unit)
     invalidate()
 }
