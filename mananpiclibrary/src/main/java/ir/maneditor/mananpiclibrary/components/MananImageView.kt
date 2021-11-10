158574cb36448f2efb65cb3c2ee29791d0b09b16
Mohammad3125 <farsroid312@gmail.com>
2021-11-10 15:21:55 +0330

    refactor: remove imageview texturing since porter-duff implemented

--- mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/components/MananImageView.kt
+++ mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/components/MananImageView.kt
@@ -1,18 +1,16 @@
 package ir.maneditor.mananpiclibrary.components
 
 import android.content.Context
-import android.graphics.*
+import android.graphics.ColorFilter
 import android.util.AttributeSet
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.view.updateLayoutParams
 import ir.maneditor.mananpiclibrary.properties.Filterable
 import ir.maneditor.mananpiclibrary.properties.Scalable
-import ir.maneditor.mananpiclibrary.properties.Texturable
-import ir.maneditor.mananpiclibrary.utils.invalidateAfter
 
 class MananImageView(context: Context, attr: AttributeSet?) : AppCompatImageView(context, attr),
-    Scalable, Filterable, Texturable {
+    Scalable, Filterable {
     constructor(context: Context) : this(context, null)
 
     private var isInitialScaling = true
@@ -21,12 +19,6 @@ class MananImageView(context: Context, attr: AttributeSet?) : AppCompatImageView
     private var minimumScalingWidth = 0
     private var minimumScalingHeight = 0
     private lateinit var imageParent: ViewGroup
-
-    private var bitmapTexture: Bitmap? = null
-
-    private val bitmapTexturePaint by lazy {
-        Paint(Paint.ANTI_ALIAS_FLAG)
-    }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
@@ -64,43 +56,4 @@ class MananImageView(context: Context, attr: AttributeSet?) : AppCompatImageView
     override fun removeFilter() {
         clearColorFilter()
     }
-
-    override fun applyTexture(bitmap: Bitmap, opacity: Float) {
-        applyTexture(bitmap, Shader.TileMode.REPEAT, opacity)
-    }
-
-    override fun applyTexture(bitmap: Bitmap, tileMode: Shader.TileMode, opacity: Float) {
-        invalidateAfter {
-            bitmapTexturePaint.colorFilter =
-                ColorMatrixColorFilter(
-                    floatArrayOf(
-                        1f, 0f, 0f, 0f, 0f,
-                        0f, 1f, 0f, 0f, 0f,
-                        0f, 0f, 1f, 0f, 0f,
-                        0f, 0f, 0f, opacity, 0f
-                    )
-                )
-            bitmapTexture = bitmap
-        }
-    }
-
-    override fun removeTexture() {
-        invalidateAfter {
-            bitmapTexturePaint.apply {
-                colorFilter = null
-                bitmapTexture = null
-            }
-        }
-    }
-
-
-    override fun dispatchDraw(canvas: Canvas?) {
-        super.dispatchDraw(canvas)
-        if (bitmapTexture != null)
-            canvas!!.apply {
-                drawBitmap(bitmapTexture!!, 0f, 0f, bitmapTexturePaint)
-            }
-    }
-
-
 }
