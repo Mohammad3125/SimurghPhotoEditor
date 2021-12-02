e3732735314b4c549e5e3eb72c158d39d798badb
Mohammad3125 <farsroid312@gmail.com>
2021-12-02 13:41:08 +0330

    refactor: remove redundant extension functions

--- mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/utils/CustomViewHelperFunctions.kt
+++ mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/utils/CustomViewHelperFunctions.kt
@@ -1,10 +1,8 @@
 package ir.maneditor.mananpiclibrary.utils
 
 import android.content.res.Resources
-import android.graphics.Canvas
 import android.util.TypedValue
 import android.view.View
-import android.view.ViewGroup
 
 private val displayMetrics = Resources.getSystem().displayMetrics
 
@@ -19,42 +17,6 @@ val Number.sp: Float
  */
 val Number.dp: Float
     get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,this.toFloat(), displayMetrics)
-
-
-/**
- * Calculates the center of canvas in X coordinates.
- */
-val Canvas.centerX
-    get() = (this.width / 2)
-
-
-/**
- * Calculates the center of canvas in Y coordinate.
- */
-val Canvas.centerY get() = (this.height / 2)
-
-/**
- * This property returns center of a ViewGroup in horizontal axis (ConstrainLayout,RelativeLayout,FrameLayout and etc.)
- */
-private val ViewGroup.centerX get() = (this.measuredWidth * 0.5f)
-
-
-/**
- * This property returns center of a ViewGroup in vertical axis (ConstrainLayout,RelativeLayout,FrameLayout and etc.)
- */
-private val ViewGroup.centerY get() = (this.measuredHeight * 0.5f)
-
-
-/**
- * This property returns the center point of a view in x direction.
- */
-val View.centerX get() = (this.measuredWidth * 0.5f)
-
-/**
- * This property returns the center point of a view in y direction.
- */
-val View.centerY get() = (this.measuredHeight * 0.5f)
-
 
 /**
  * This extension function invokes a block and invalidate the view afterwards.
