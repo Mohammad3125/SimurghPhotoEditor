58c1d224849f1eb7d434da0093562fa6fb97e63f
Mohammad3125 <farsroid312@gmail.com>
2021-12-19 10:47:06 +0330

    refactor: migrate all components to use new dimension conversion functions

--- mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/utils/CustomViewHelperFunctions.kt
+++ mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/utils/CustomViewHelperFunctions.kt
@@ -4,22 +4,6 @@ import android.content.res.Resources
 import android.util.TypedValue
 import android.view.View
 
-private val displayMetrics = Resources.getSystem().displayMetrics
-
-/**
- * This extension property converts an SP number into pixels.
- */
-@Deprecated("Use new extension function instead")
-val Number.sp: Float
-    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this.toFloat(), displayMetrics)
-
-/**
- * This extension property converts a DP number into pixels.
- */
-@Deprecated("Use new extension function instead")
-val Number.dp: Float
-    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), displayMetrics)
-
 /**
  * Extension function for converting a dp value to pixels.
  * @param number Number to be converted.
