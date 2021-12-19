f7f5bfeea4b6f37c60b07bf3bb12f5bc891f85fb
Mohammad3125 <farsroid312@gmail.com>
2021-12-19 10:30:53 +0330

    fix: bug in dimension conversion where used global resources not context resources
    If we use global resources then real dimensions of current devices is not in that and we should use resources of a context inside application to get real device display metrics

--- mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/utils/CustomViewHelperFunctions.kt
+++ mananpiclibrary/src/main/java/ir/maneditor/mananpiclibrary/utils/CustomViewHelperFunctions.kt
@@ -9,14 +9,38 @@ private val displayMetrics = Resources.getSystem().displayMetrics
 /**
  * This extension property converts an SP number into pixels.
  */
+@Deprecated("Use new extension function instead")
 val Number.sp: Float
-    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,this.toFloat(), displayMetrics)
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, this.toFloat(), displayMetrics)
 
 /**
  * This extension property converts a DP number into pixels.
  */
+@Deprecated("Use new extension function instead")
 val Number.dp: Float
-    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,this.toFloat(), displayMetrics)
+    get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), displayMetrics)
+
+/**
+ * Extension function for converting a dp value to pixels.
+ * @param number Number to be converted.
+ */
+fun View.dp(number: Number): Float {
+    val metric =
+        if (context != null) context.resources.displayMetrics else Resources.getSystem().displayMetrics
+
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, number.toFloat(), metric)
+}
+
+/**
+ * Extension function for converting a sp number to pixels.
+ * @param number Number to be converted.
+ */
+fun View.sp(number: Number): Float {
+    val metric =
+        if (context != null) context.resources.displayMetrics else Resources.getSystem().displayMetrics
+
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, number.toFloat(), metric)
+}
 
 /**
  * This extension function invokes a block and invalidate the view afterwards.
