c06a3ff7db15d104a59c6acac958e8bc1e60281f
Mohammad3125 <farsroid312@gmail.com>
2022-01-02 15:52:47 +0330

    feat: make a factory class for creating components with required layout paramsM

---
+++ mananpiclibrary/src/main/java/ir/manan/mananpic/utils/MananFactory.kt
@@ -0,0 +1,42 @@
+package ir.manan.mananpic.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.widget.FrameLayout
+import ir.manan.mananpic.components.MananTextView
+import ir.manan.mananpic.components.imageviews.MananImageView
+
+/**
+ * A factory class responsible for creating components like [MananTextView], [MananImageView].
+ */
+class MananFactory {
+    companion object {
+        /**
+         * Creates a [MananImageView] with required layout params.
+         */
+        fun createImageView(context: Context, bitmap: Bitmap): MananImageView {
+            return MananImageView(context).apply {
+                layoutParams = FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.MATCH_PARENT,
+                    FrameLayout.LayoutParams.MATCH_PARENT
+                )
+                setImageBitmap(bitmap)
+            }
+        }
+
+        /**
+         * Creates a [MananTextView] with required layout params.
+         */
+        fun createTextView(context: Context, text: String): MananTextView {
+            return MananTextView(context).apply {
+                layoutParams = FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT
+                )
+                this.text = text
+                setTextColor(Color.BLACK)
+            }
+        }
+    }
+}
