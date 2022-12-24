7860709480e45bffa6e3fd4168ab0495cf8cca74
Mohammad3125 <farsroid312@gmail.com>
2022-12-24 08:15:46 +0330

    feat: now BrushSelector.kt draws continuous lines

--- mananpiclibrary/src/main/java/ir/manan/mananpic/components/selection/selectors/BrushSelector.kt
+++ mananpiclibrary/src/main/java/ir/manan/mananpic/components/selection/selectors/BrushSelector.kt
@@ -12,7 +12,11 @@ class BrushSelector : PathBasedSelector() {
     // Paint for circles in path.
     private val brushPaint by lazy {
         Paint(Paint.ANTI_ALIAS_FLAG).apply {
-            style = Paint.Style.FILL
+            style = Paint.Style.STROKE
+            strokeCap = Paint.Cap.ROUND
+            strokeJoin = Paint.Join.ROUND
+            strokeMiter = 20f
+            strokeWidth = 32f
         }
     }
 
@@ -51,6 +55,25 @@ class BrushSelector : PathBasedSelector() {
             invalidate()
         }
 
+    private var perv1x = 0f
+    private var perv1y = 0f
+
+    private var mid1x = 0f
+    private var mid1y = 0f
+
+    private var mid2x = 0f
+    private var mid2y = 0f
+
+    private var perv2x = 0f
+    private var perv2y = 0f
+
+    private var curX = 0f
+    private var curY = 0f
+
+    private var isFirstThreeCreated = false
+
+    private var counter = 0
+
     override fun shouldParentTransformDrawings(): Boolean {
         return true
     }
@@ -61,22 +84,67 @@ class BrushSelector : PathBasedSelector() {
     }
 
     override fun onMoveBegin(initialX: Float, initialY: Float) {
-
     }
 
     override fun onMove(dx: Float, dy: Float, ex: Float, ey: Float) {
-        drawCircles(ex, ey)
+        if (!isFirstThreeCreated) {
+
+            when (counter) {
+                0 -> {
+                    perv2x = ex
+                    perv2y = ey
+                }
+                1 -> {
+                    perv1x = ex
+                    perv1y = ey
+                }
+                else -> {
+                    curX = ex
+                    curY = ey
+
+                    calculateAndDrawQuad()
+
+                    counter = 0
+
+                    isFirstThreeCreated = true
+
+                    return
+                }
+            }
+
+            counter++
+        } else {
+            perv2x = perv1x
+            perv2y = perv1y
+
+            perv1x = curX
+            perv1y = curY
+
+            curX = ex
+            curY = ey
+
+            calculateAndDrawQuad()
+        }
     }
 
     override fun onMoveEnded(lastX: Float, lastY: Float) {
-        drawCircles(lastX, lastY)
-
         paths.add(Path(path))
+        isFirstThreeCreated = false
     }
 
-    private fun drawCircles(touchX: Float, touchY: Float) {
-        path.addCircle(touchX, touchY, brushSize, Path.Direction.CCW)
+    private fun calculateAndDrawQuad() {
+        mid1x = (perv1x + perv2x) * 0.5f
+        mid1y = (perv1y + perv2y) * 0.5f
+
+        mid2x = (curX + perv1x) * 0.5f
+        mid2y = (curY + perv1y) * 0.5f
+
+        path.moveTo(mid1x, mid1y)
+
+        path.quadTo(perv1x, perv1y, mid2x, mid2y)
+
         invalidate()
+
         isPathClose = true
     }
 
